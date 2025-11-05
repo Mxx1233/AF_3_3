ba5db43056aefa4284d0c1338dbab9b2f6a22c51
Shengtao Wen <shengtao.wen@stud.tu-darmstadt.com>
2025-11-05 21:21:14 +0100

    Revert "Initial commit from ws-template"
    This reverts commit 16cfba24320e597bac248fe37d96cab4264e8fa7.

--- ws-template/ci_install.sh
+++
@@ -1,9 +0,0 @@
-# this script must install all necessary components to build and test
-# the code within this repository and that are not contained in the
-# base docker image used by the CI
-curl --header "JOB-TOKEN: $CI_JOB_TOKEN" "https://git-ce.rwth-aachen.de/api/v4/projects/1881/packages/generic/debian/2.3.3/ros-jazzy-psaf-ucbridge-msgs_2.3.3-0noble_amd64.deb" --output ros-jazzy-psaf-ucbridge-msgs_2.3.3-0noble_amd64.deb
-curl --header "JOB-TOKEN: $CI_JOB_TOKEN" "https://git-ce.rwth-aachen.de/api/v4/projects/1881/packages/generic/debian/2.3.3/ros-jazzy-psaf-ucbridge_2.3.3-0noble_amd64.deb" --output ros-jazzy-psaf-ucbridge_2.3.3-0noble_amd64.deb
-dpkg -i ros-jazzy-psaf-ucbridge-msgs_2.3.3-0noble_amd64.deb
-dpkg -i ros-jazzy-psaf-ucbridge_2.3.3-0noble_amd64.deb
-rm ros-jazzy-psaf-ucbridge-msgs_2.3.3-0noble_amd64.deb
-rm ros-jazzy-psaf-ucbridge_2.3.3-0noble_amd64.deb
